396f62549656294a752f3a80283b16b53a464e57
zhfrnn <zhafran.ph@gmail.com>
2022-12-02 19:55:42 +0700

    frist commit

---
+++ js/main.js
@@ -0,0 +1,35 @@
+const about = document.querySelector("#about");
+const contact = document.querySelector("#contact");
+const aboutContent = document.querySelector("#about-content");
+const contactContent = document.querySelector("#contact-content");
+
+about.addEventListener("click", () => {
+  const aboutBox = new WinBox({
+    title: "About Me",
+    width: "400px",
+    height: "400px",
+    mount: aboutContent,
+    onfocus: function () {
+      this.setBackground("#00aa00");
+    },
+    onblur: function () {
+      this.setBackground("#777");
+    },
+  });
+});
+
+contact.addEventListener("click", () => {
+  const conacttBox = new WinBox({
+    title: "Contact Me",
+    background: "#00aa00",
+    width: "400px",
+    height: "400px",
+    mount: contactContent,
+    onfocus: function () {
+      this.setBackground("#00aa00");
+    },
+    onblur: function () {
+      this.setBackground("#777");
+    },
+  });
+});
